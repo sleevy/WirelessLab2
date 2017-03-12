a98dd775f04edbd059eebd6bc7ae7ab183788000
sleevy <chirocam@gmail.com>
2017-03-12 17:11:23 -0400

    more or less done. added cardview and some other things

--- app/src/main/java/edu/georgiasouthern/cr04956/wirelesslab2/MainActivity.java
+++ app/src/main/java/edu/georgiasouthern/cr04956/wirelesslab2/MainActivity.java
@@ -2,12 +2,16 @@ package edu.georgiasouthern.cr04956.wirelesslab2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
+import android.provider.ContactsContract;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +19,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import static edu.georgiasouthern.cr04956.wirelesslab2.MainActivity.CountryAdapter.VIEW_HEIGHT;
 
 
 public class MainActivity extends Activity {
@@ -24,6 +30,7 @@ public class MainActivity extends Activity {
 
     private RecyclerView countryRecycler;
     private CountryStorage cs;
+    private boolean isCardView = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +48,7 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Log.i("Reset onClick", "Resetting country list");
                 cs.resetCountryList();
+                updateUIType();
                 countryRecycler.getAdapter().notifyDataSetChanged();
             }
         });
@@ -54,6 +62,7 @@ public class MainActivity extends Activity {
         countryRecycler.setAdapter(adapter);
 
         //todo change to card layout if few in list
+       decor = new GridSpacingItemDecoration(2, dpTopx(10), true);
 
     }
 
@@ -67,7 +76,10 @@ public class MainActivity extends Activity {
 
     protected class CountryAdapter extends RecyclerView.Adapter<CountryHolder> {
         public static final int VIEW_HEIGHT = 100;
-
+        public static final int CARD_WIDTH = 210;
+        public static final int CARD_HEIGHT = 500;
+        public static final int HORIZONTAL_MARGIN = 10;
+        public static final int VERT_MARGIN = 10;
         public CountryAdapter() {
         }
 
@@ -78,13 +90,29 @@ public class MainActivity extends Activity {
 
         @Override
         public CountryHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LinearLayout layout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_country, parent, false);
-            //size,margin,padding,layout params
-            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-            int pxHeight = (int)((VIEW_HEIGHT * displayMetrics.density) + 0.5);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,pxHeight);
-            layout.setLayoutParams(params);
+            LinearLayout layout;
+            LinearLayout.LayoutParams params;
+            if(isCardView) {
+                layout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_country_card, parent, false);
+//                int pxHeight = dpTopx(CARD_HEIGHT);
+//                int pxWidth = dpTopx(CARD_WIDTH);
+//                params = new LinearLayout.LayoutParams(pxWidth, pxHeight);
+//                params.leftMargin = HORIZONTAL_MARGIN;
+//                params.rightMargin = HORIZONTAL_MARGIN;
+//                params.topMargin = VERT_MARGIN;
+//                params.bottomMargin = VERT_MARGIN;
+//                layout.setLayoutParams(params);
 
+                layout.setGravity(Gravity.CENTER);
+
+            } else {
+                layout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_country, parent, false);
+
+                int pxHeight = dpTopx(VIEW_HEIGHT);
+                params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, pxHeight);
+
+                layout.setLayoutParams(params);
+            }
             layout.setOnClickListener(new CountryListener());
 
             CountryHolder ch = new CountryHolder(layout);
@@ -114,24 +142,90 @@ public class MainActivity extends Activity {
             startActivityForResult(toCountryActivity, 1);
         }
     }
-
+    private GridSpacingItemDecoration decor;
+    public void updateUIType() {
+        boolean wasCardView = isCardView;
+        isCardView = cs.size() <= 4;
+        if(wasCardView == isCardView) {
+            return;
+        }
+        if(isCardView) {
+            GridLayoutManager man = new GridLayoutManager(this, 2);
+//            man.offsetChildrenHorizontal(10);
+//            man.offsetChildrenVertical(5);
+            countryRecycler.addItemDecoration(decor);
+            countryRecycler.setLayoutManager(man);
+        } else {
+            countryRecycler.removeItemDecoration(decor);
+            countryRecycler.setLayoutManager(new LinearLayoutManager(this));
+        }
+    }
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        updateUIType();
         countryRecycler.getAdapter().notifyDataSetChanged();
     }
 
+    private int dpTopx(int dp) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        return (int) ((dp * displayMetrics.density) + 0.5);
+    }
     private class CountryHolder extends RecyclerView.ViewHolder {
             public ImageView flagImage;
             public TextView countryName;
             public TextView continentName;
         public CountryHolder(View itemView) {
             super(itemView);
-            flagImage = (ImageView) itemView.findViewById(R.id.itemCountryFlag);
-            countryName = (TextView) itemView.findViewById(R.id.itemCountryName);
-            continentName = (TextView) itemView.findViewById(R.id.txtContinent);
+            if(isCardView) {
+                flagImage = (ImageView) itemView.findViewById(R.id.imgCardFlag);
+                countryName = (TextView) itemView.findViewById(R.id.txtCardCountryName);
+                continentName = (TextView) itemView.findViewById(R.id.txtCardContinent);
+            } else {
+                flagImage = (ImageView) itemView.findViewById(R.id.itemCountryFlag);
+                countryName = (TextView) itemView.findViewById(R.id.itemCountryName);
+                continentName = (TextView) itemView.findViewById(R.id.txtContinent);
+            }
         }
 
 
     }
 
+    /**
+     * http://www.androidhive.info/2016/05/android-working-with-card-view-and-recycler-view/
+     * this code was written, presumably, by Ravi Tamada and is available at tbe above link
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
 
 }
